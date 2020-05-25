970298435c4a3ebe66d67cb36603d7201df422d9
Andy Grabow <andy@freilandkiwis.de>
2020-05-25 20:35:01 +0200

    .

---
+++ docs/demo.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+html(lang="en")
+  head
+    title dotfiles
+    meta(name="viewport" content="width=device-width, initial-scale=1")
+  
+  body
+    | blubber
+    .kasten blub
+</template>
+
+<style lang="sass">
+body 
+  background: lightgreen
+  color: green
+  font-size: 30px
+
+.kasten
+  height: 100px
+  width: 100px
+  background: red
+  display: block
+</style>
---
+++ docs/test.vue
@@ -0,0 +1,50 @@
+<template lang="pug">
+html(lang="en")
+  head
+    title dotfiles
+    meta(name="viewport" content="width=device-width, initial-scale=1")
+  
+  body
+    | blubber
+    .kasten blub
+</template>
+
+<style lang="sass">
+
+@font-face
+  font-family: SauceCodePro
+  src: url(SauceCodePro.ttf)
+
+body, .asciinema-terminal 
+  font-family: "SauceCodePro"
+  
+.asciinema-theme-asciinema 
+  .fg-1
+    color: #AB6053
+  .fg-2
+    color: #70865B
+  .fg-4
+    color: #5E70A4
+  .fg-6
+    color: #A99367
+  .fg-9
+    color: #AB2C1D
+  .fg-10
+    color: #44973C
+  .bg-52
+    background: #571106
+  .bg-22
+    background: #275D17
+  .fg-146
+    color: #AFB0D4
+  .bg-1
+    background: #AB6052
+  .fg-12
+    color: #90ACF3
+  .bg-2
+    background: #70875B
+  .bg-3
+    background: #A67542
+  .bg-4
+    background: #5D70A5
+</style>
