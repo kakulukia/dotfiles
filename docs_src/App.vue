f5725bc8eaca1d271b4467a41c5020814631febd
Andy Grabow <andy@freilandkiwis.de>
2020-06-05 10:29:53 +0200

    enhanced feqture listing

--- docs_src/App.vue
+++ docs_src/App.vue
@@ -1,15 +1,20 @@
 <template lang="pug">
   body
-    .container.is-hidden-desktop
+    .container.is-hidden-tablet
       .section
         h1.title dotfiles
         p.
           This is a not meant for mobile .. please come back with a higher resolution. :)
-    .container.is-hidden-touch
+        br
+        p.
+          Well, the shell recording won't fit on your screen, so you should at least
+          enable desktop mode. But even then phones are kinda small. :/
+    .container.is-hidden-mobile
       .section
         h1.title dotfiles
       .section
         asciinema-player(v-pre id="player" src="demo.cast" poster="npt:0:01" speed="1.5" idle-time-limit="1")
+
         h2.subtitle Intro
         p.
           For the last 5 years I have been building up my shell environment and its the
@@ -18,6 +23,7 @@
         p.
           Some of this stuff I didn't even know was possible before I switched to ZSH or
           found app X, so i just wanted to share the whole collection.
+
         h2.subtitle Installation
         p.
           Be sure you are logged in as the user for whom you want to install this shell
@@ -56,6 +62,51 @@
 
         p The dotfiles repo can be updated with the #[span.command update-dotfiles] alias.
 
+        h2.subtitle Other features
+        ul
+          li suggestions (grey text) - use the right arrow key to accept
+          li history reverse search - use the up arrow to cycle through previous commands
+            | Anything you typed before hitting up will be used as filter and highlighted.
+            br
+            br
+            | migr + up
+            img(src="history.png")
+            | This seriously saves a lot of typing. 😇
+          li red / green colored commands - command is (un)available
+          li mv - no need to type the file name twice for renaming
+          li top - is aliased to glances if installed
+          li ctop - for docker containers
+          li ,, - jump to the git root dir
+          li o - will open the finder in the current directory
+          li cd to.app - the is an app in the bin folder that provides a
+            |  way to reverse the trick an open a terminal at the current finder location.
+            |  Use the command key to just drag it into the finders toolbar.
+            br
+            br
+            | The result will look like this:
+            img(src="cdto.png")
+          li tm - there's a tmux theme and tm will reconnect to to your last session o create a new one
+            br
+            | I also changed the default prefix to CTRL+SPACE for easier usage.
+          li diff - aliased to diff-so-fancy in general, not just the git version
+          li co - use fasd to launch any known file in VS Code
+          li errorcode - in general I don't see the point in displaying the error code
+            | , hence the sad smiley in the prompt, but if you really wanna know, use that alias (unless you can remember the #?)
+          li ips - will show all local ips (IPV4) / ip will show some info about your external one
+          li prettyping - fancier ping
+          li go-reload - hot reload for go apps you are working on
+          li mgs - multi git status - if your company/current project totally embraces micro services (🙈)
+            |  this might come in handy to update em all in one go
+          li rg - alias for "rg -S --max-columns 444" won't clutter the screen with nasty one line files
+
+
+        h2.subtitle ToDo
+        ul
+          li colorls is a lil slow - there is a promising alternative, but
+            a(hreaf="https://github.com/Peltoche/lsd/issues/92")  it's not looking sexy yet
+          li Maybe some day in the future I'm switching yet again to prezto and will redo my default theme.
+            |  But as of yet this still sounds like too much trouble. :/
+
 
 </template>
 
@@ -82,6 +133,10 @@ body, .asciinema-terminal
 
 .section + .section
   padding-top: 0
+
+img
+  display: block
+  margin: 1em auto
 
 span.command
   background: #eee
