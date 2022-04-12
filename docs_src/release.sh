f63a3d11ef6d84290dd71c2183d90f64a29111f1
Andy <andy@freilandkiwis.de>
2022-04-12 12:13:23 +0200

    readme updates

--- docs_src/release.sh
+++ docs_src/release.sh
@@ -1,5 +1,6 @@
 vue build && rm -rf ../docs && mv dist ../docs
 gsed -i 's#="/#="#g' ../docs/index.html # make the included files work for localhost as well as at /dotfiles/
+gsed -i 's#/fonts#fonts#g' ..docs/css/*
 git add ../docs ../docs_src
 git commit -m "readme updates"
 git push
