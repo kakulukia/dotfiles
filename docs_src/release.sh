2ecaa3e3df2c570faeb9786435c701fb9654382b
Andy <andy@freilandkiwis.de>
2022-04-12 12:03:12 +0200

    fixed docs release script

--- docs_src/release.sh
+++ docs_src/release.sh
@@ -1,4 +1,5 @@
 vue build && rm -rf ../docs && mv dist ../docs
+gsed -i 's#=/#=#g' ../docs/index.html # make the included files work for localhost as well as at /dotfiles/
 git add ../docs ../docs_src
 git commit -m "readme updates"
 git push
